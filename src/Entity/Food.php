6076b1088c49022e5f85ca174f999321a17aaeff
José Carlos Liébana <jcliebana@dapda.com>
2020-10-27 22:17:25 +0100

    Created first entities

---
+++ src/Entity/Food.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FoodRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FoodRepository::class)
+ */
+class Food
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=90)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $calories = 0;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $carbohydrates = 0;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $protein = 0;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $fats = 0;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $sugars = 0;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2, options={"default" : 0})
+     */
+    private $fiber = 0;
+
+    /**
+     * @ORM\Column(type="string", options={"default" : null})
+     */
+    private $image;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getCalories(): ?string
+    {
+        return $this->calories;
+    }
+
+    public function setCalories(string $calories): self
+    {
+        $this->calories = $calories;
+
+        return $this;
+    }
+
+    public function getCarbohydrates(): ?string
+    {
+        return $this->carbohydrates;
+    }
+
+    public function setCarbohydrates(string $carbohydrates): self
+    {
+        $this->carbohydrates = $carbohydrates;
+
+        return $this;
+    }
+
+    public function getProtein(): ?string
+    {
+        return $this->protein;
+    }
+
+    public function setProtein(string $protein): self
+    {
+        $this->protein = $protein;
+
+        return $this;
+    }
+
+    public function getFats(): ?string
+    {
+        return $this->fats;
+    }
+
+    public function setFats(string $fats): self
+    {
+        $this->fats = $fats;
+
+        return $this;
+    }
+
+    public function getSugars(): ?string
+    {
+        return $this->sugars;
+    }
+
+    public function setSugars(string $sugars): self
+    {
+        $this->sugars = $sugars;
+
+        return $this;
+    }
+
+    public function getFiber(): ?string
+    {
+        return $this->fiber;
+    }
+
+    public function setFiber(string $fiber): self
+    {
+        $this->fiber = $fiber;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+
+}
